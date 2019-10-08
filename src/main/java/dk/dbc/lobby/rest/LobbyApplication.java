ba845f62f396f2671b816732edbf0d70d0ff3326
Jan Bauer Nielsen <jbn@dbc.dk>
2019-10-08 09:26:01 +0200

    Add status endpoint
    Auditors: atm, jbr

---
+++ src/main/java/dk/dbc/lobby/rest/LobbyApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.lobby.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class LobbyApplication extends Application {
+    private static final Set<Class<?>> classes = new HashSet<>();
+    static {
+        classes.add(Status.class);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+}
